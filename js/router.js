f5e67491b6fddca7cba68a77edc885392a65562c
Beachkila <kbmallonee@aol.com>
2026-07-27 00:37:23 -0700

    Rename CMYK route to cmyk

--- js/router.js
+++ js/router.js
@@ -11,7 +11,7 @@ const Router = {
     edit:      (v, arg) => Mine.uploadForm(v, arg),
     collected: (v)      => Collected.render(v),
     negatives: (v)      => Negatives.render(v),
-    CMYK: (v)      => CMYK.render(v),
+    cmyk: (v)      => cmyk.render(v),
     account:   (v)      => Mine.account(v)
   },
 
