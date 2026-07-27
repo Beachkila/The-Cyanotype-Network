4224d4431d4f275ed6e53bce68f26bda379d6dfb
Beachkila <kbmallonee@aol.com>
2026-07-27 01:08:20 -0700

    Delete cmyk.js

--- cmyk.js
+++
@@ -1,9 +0,0 @@
-// THE CYANOTYPE NETWORK · cmyk.js — 
-const Negatives = {
-  render(view) {
-    view.innerHTML =
-      '<iframe title="cmyk" src="cmyk.html" ' +
-      'style="display:block;width:100%;height:calc(100vh - 150px);' +
-      'min-height:620px;border:0;background:transparent"></iframe>';
-  }
-};
